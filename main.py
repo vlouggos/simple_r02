6d13e68f8dacf6ca69497f5890127ee0df638ba9
vlouggos <vlouggos@enafsitc.gr>
2022-12-31 21:27:22 +0200

    Commit 7

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,  request, redirect, url_for
 import os
 import psycopg2
-from flask-sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config('SQLALCHEMY_DATABASE_URI') = os.environ.get("DATABASE_URL")
+DATABASE_URL = os.environ.get("DATABASE_URL")
+app.config('SQLALCHEMY_DATABASE_URI') = DATABASE_URL
 db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return f"Base url: {DATABASE_URL}"
 
 
 if __name__ == "__main__":
