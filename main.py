8fac27d6b6843506043b098d40904344c4ec58c0
vlouggos <vlouggos@enafsitc.gr>
2023-01-01 00:53:27 +0200

    Commit 8

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 #DATABASE_URL = os.environ.get("DATABASE_URL")
-app.config('SQLALCHEMY_DATABASE_URI') = "postgres://dnmfcrwhuxbdpw:16516902f9e5a3cce0305ae4258177d6be7fd060c4e5dbd5740bfc7ba571755f@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/d17512vorjj707"
+app.config('SQLALCHEMY_DATABASE_URI') = postgres://dnmfcrwhuxbdpw:16516902f9e5a3cce0305ae4258177d6be7fd060c4e5dbd5740bfc7ba571755f@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/d17512vorjj707
 
 db = SQLAlchemy(app)
 
