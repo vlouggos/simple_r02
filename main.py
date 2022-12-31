2c99c539e6c83ff6127fb969bdbe91f26af72170
vlouggos <vlouggos@enafsitc.gr>
2022-12-31 21:14:06 +0200

    Commit 7

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
+import os
+import psycopg2
+from flask-sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+app.config('SQLALCHEMY_DATABASE_URI') = os.environ.get("DATABASE_URL")
+db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
