8de08a43b0d61a20581f3d35bfd636acad57ca6a
vlouggos <vlouggos@enafsitc.gr>
2022-12-31 21:36:11 +0200

    Commit 8

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+import binary as binary
 from flask import Flask, render_template,  request, redirect, url_for
 import os
 import psycopg2
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-DATABASE_URL = os.environ.get("DATABASE_URL")
-app.config('SQLALCHEMY_DATABASE_URI') = DATABASE_URL
+#DATABASE_URL = os.environ.get("DATABASE_URL")
+app.config('SQLALCHEMY_DATABASE_URI') = "postgres://dnmfcrwhuxbdpw:16516902f9e5a3cce0305ae4258177d6be7fd060c4e5dbd5740bfc7ba571755f@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/d17512vorjj707"
+
 db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
-    return f"Base url: {DATABASE_URL}"
+    return "Base url: {DATABASE_URL}"
 
 
 if __name__ == "__main__":
